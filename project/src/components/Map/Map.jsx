1ed2465546610933159afc30b305ca39b7b22c86
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-02-24 23:34:05 +0530

    Update map

--- project/src/components/Map/Map.jsx
+++ project/src/components/Map/Map.jsx
@@ -141,9 +141,9 @@ const Map = () => {
         >
           <ZoomControl position="bottomright" />
           <TileLayer
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            maxZoom={20}
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            maxZoom={19}
           />
           
           {mapData.map((location, index) => (
